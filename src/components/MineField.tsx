39b3722e176cd79a12d8b2af123f37412e98c334
Paulo Dias <paulosvdd05@gmail.com>
2023-03-21 10:06:47 -0300

    MineField

--- src/components/MineField.tsx
+++ src/components/MineField.tsx
@@ -3,5 +3,18 @@ import { View, StyleSheet } from "react-native";
 import Field from "./Field";
 
 export default props => {
-    const rows = props.board
+    const rows = props.board.map((row, r) => {
+        const columns = row.map((field, c) => {
+            return <Field {...field} key={c} />
+        })
+        return <View key={r}>{columns}</View>
+    })
+    return <View style={styles.container}>{rows}</View>
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: 'row',
+        backgroundColor: '#EEE',
+    }
+})
