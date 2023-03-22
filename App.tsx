dd93aa726e07e243e2c1eb710a963e02587cfb77
Paulo Dias <paulosvdd05@gmail.com>
2023-03-22 10:36:19 -0300

    Selecionar Dificuldade

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Platform, StyleSheet, Text, View, Alert } from "react-native";
 import params from "./src/params";
 import MineField from "./src/components/MineField";
 import Header from "./src/components/Header";
+import LevelSelection from "./src/screens/LevelSelection";
 import{
   createMinedBoard,
   cloneBoard,
@@ -34,7 +35,8 @@ export default class App extends Component {
     return{
       board: createMinedBoard(rows, cols, this.minesAmount()),
       won: false,
-      lost: false
+      lost: false,
+      showLevelSelection: false,
     }
   }
 
@@ -66,11 +68,23 @@ export default class App extends Component {
 
     this.setState({board, won})
   }
+
+  onLevelSelected = level =>{
+    params.difficultLevel = level
+    this.setState(this.createState())
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <LevelSelection isVisible={this.state.showLevelSelection}
+          onLevelSelected={this.onLevelSelected}
+          onCancel={() => this.setState({showLevelSelection: false})} />
 
-        <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)} onNewGame={()=>this.setState(this.createState)} />
+        <Header 
+        flagsLeft={this.minesAmount() - flagsUsed(this.state.board)} o
+        nNewGame={()=>this.setState(this.createState)}
+        onFlagPress={() => this.setState({showLevelSelection: true})} />
 
         <View style={styles.board}>
           <MineField board={this.state.board} 
